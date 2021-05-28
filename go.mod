1661bcbd61096a703742577561448998e7569ff0
norbjd <norbjd@users.noreply.github.com>
2021-05-28 14:03:13 +0200

    ✨ Log every request

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/norbjd/simple-images-api
 go 1.16
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.8.1
 )
--- router.go
+++ router.go
@@ -3,8 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func handlerWithLogging(handler func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return handlers.LoggingHandler(
+		log.StandardLogger().Writer(),
+		http.HandlerFunc(handler),
+	)
+}
 
 func handlerNotImplemented(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(501)
@@ -14,9 +24,12 @@ func handlerNotImplemented(w http.ResponseWriter, r *http.Request) {
 func InitRouter() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/images", handlerNotImplemented).Methods("GET", "POST")
-	router.HandleFunc("/images/{imageID}", handlerNotImplemented).Methods("GET", "DELETE")
-	router.HandleFunc("/images/{imageID}/metadata", handlerNotImplemented).Methods("GET")
+	router.Handle("/images", handlerWithLogging(handlerNotImplemented)).Methods("GET", "POST")
+	router.Handle("/images/{imageID}", handlerWithLogging(handlerNotImplemented)).Methods("GET", "DELETE")
+	router.Handle("/images/{imageID}/metadata", handlerWithLogging(handlerNotImplemented)).Methods("GET")
+	router.PathPrefix("/").Handler(handlerWithLogging(func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	}))
 
 	return router
 }
